050f631b5c188bcbe690a2ed4127af16fd3e34e8
iliyqqq <zapaska200532@gmail.com>
2025-04-23 15:37:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggle-interests');
     const interestCards = document.querySelectorAll('.interest-card');
     
-    // Изначально скрываем карточки
     interestCards.forEach(card => {
         card.classList.add('hidden');
     });
@@ -12,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
             card.classList.toggle('hidden');
         });
         
-        // Меняем текст кнопки
         if (toggleButton.textContent === 'Показать интересы') {
             toggleButton.textContent = 'Скрыть интересы';
         } else {
